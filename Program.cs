77b4442dfd1a71fbbdfe1150d8cf1cab4fb23e1a
ilia <115416335+Iliathebest@users.noreply.github.com>
2022-10-19 12:24:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4 8 -> 2, 4, 6, 8
-Console.WriteLine("enter number");
-int number = Convert.ToInt32(Console.ReadLine());
- int numberMin = 2;
-while(numberMin <= number)
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньше
+//  a = 5; b = 7 -> max = 7
+//   a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("enter number1");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1>number2)
 {
-    Console.WriteLine(numberMin);
-numberMin= numberMin + 2;
+Console.WriteLine("number_1 max number_2 min");
+}
+else
+{
+    Console.WriteLine("number_1min number_2 max");
 }
