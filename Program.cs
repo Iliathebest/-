55fc385b08441e900b61a5637f4e94817b60af07
ilia <115416335+Iliathebest@users.noreply.github.com>
2022-10-19 12:22:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4 8 -> 2, 4, 6, 8
+Console.WriteLine("enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+ int numberMin = 2;
+while(numberMin <= number)
+{
+    Console.WriteLine(numberMin);
+numberMin= numberMin + 2;
+}
